23aed1f5c491b40ca354efebfed42880ad6bb562
IshiiAyuka <131929503+IshiiAyuka@users.noreply.github.com>
2025-11-10 11:28:53 +0900

    update

--- Decoder/main.sh
+++ Decoder/main.sh
@@ -3,7 +3,7 @@
 source /home/slab/ishiiayuka/.pyenv/versions/anaconda3-2023.03/bin/activate
 conda activate esm2
 
-cd /home/slab/ishiiayuka/M2/Decoder
+cd /home/slab/ishiiayuka/M2
 
-nohup python -u main_train_evaluate.py >main_output_onlytrain.log 2>main_error_onlytrain.log &
+nohup python -u Decoder/main_AR.py >Decoder/output_error/output_1110.log 2>Decoder/output_error/error_1110.log &
 pid=$!
