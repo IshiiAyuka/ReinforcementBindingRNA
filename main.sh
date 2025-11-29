d22d3c56a5eed6c5b3108576fbd54e1e9fcc5232
IshiiAyuka <131929503+IshiiAyuka@users.noreply.github.com>
2025-11-29 15:43:29 +0900

    1129

--- main.sh
+++ main.sh
@@ -10,4 +10,4 @@ conda activate reinforce
 
 cd /home/slab/ishiiayuka/M2
 
-nohup python -u reinforce_swissprot_AR_offtarget.py > output_1128_1.log 2> error_1128_1.log &
+nohup python -u reinforce_swissprot_AR_offtarget_addreward.py > output_1129_4.log 2> error_1129_4.log &
